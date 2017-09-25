cdb33bba2e10b4a9aa012bac3f1d1ea98d961e05
Ignacio Castro <castroignacio888@gmail.com>
2017-09-25 11:44:56 -0300

    add search by name ctrl

--- server/controllers/weather.ts
+++ server/controllers/weather.ts
@@ -1,7 +1,31 @@
+import * as weather from 'openweathermap-js';
+
+
 export default class WeatherCtrl {
 
-  test = (req, res) => {
-    res.status(200).json({ test: 'true' });
+
+
+  byName = (req, res) => {
+
+    let city = req.query.name;
+
+
+    weather.defaults({
+      appid: '7ce20399c4214442464480835e10863a',
+      method: 'name',
+      location: city,
+      format: 'JSON',
+      accuracy: 'accurate',
+      units: 'metric'
+    });
+
+    weather.current(function(err, data) {
+      if (!err)
+        res.status(200).json(data);
+      else
+        console.error(err.message);
+    });
+
   }
 
 }
