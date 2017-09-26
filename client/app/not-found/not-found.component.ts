3172809eb51722717117991139a638582949b636
Ignacio Castro <castroignacio888@gmail.com>
2017-09-26 13:14:29 -0300

    add search when not found

--- client/app/not-found/not-found.component.ts
+++ client/app/not-found/not-found.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Router, ActivatedRoute, Params} from '@angular/router';
+
 
 @Component({
   selector: 'app-not-found',
@@ -6,6 +8,16 @@ import { Component } from '@angular/core';
 })
 export class NotFoundComponent {
 
-  constructor() { }
+  error: string;
+  city: string;
+
+  constructor(public activatedRoute: ActivatedRoute, public router: Router) { }
+  ngOnInit() {
+    this.error = this.activatedRoute.snapshot.queryParams['error'];
+  }
+
+  search() {
+    this.router.navigate([this.city]);
+  }
 
 }
